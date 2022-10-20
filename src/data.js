def1f8053fedcd4c5be60347977185afd693e8da
nikhitha <nikhitha.u@comakeit.com>
2022-10-20 11:56:11 +0530

    Firest Commit

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+      question: "Who is prime minister of India",
+      incorrectAnswers: [
+        "Rahul Gandhi",
+        "Amith Shah",
+        "Atal Bhirai Vajpay",
+      ],
+      correctAnswer: "Narendra Modi",
+    },
+  
+    {
+      question:
+        "Guess the odd one out?",
+      incorrectAnswers: [
+        "Climb",
+        "Walk",
+        "Swim",
+      ],
+      correctAnswer: "Listen",
+    },
+    {
+      question: "What is the capital of Mizoram?",
+      incorrectAnswers: ["Agartala", "Kavaratti", "	Daman"],
+      correctAnswer: "Aizawl",
+    },
+    
+    
+  ];
+  
+  export default data;
